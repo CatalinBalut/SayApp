a04add1761d7422d49be51816e02dc3574ecc8dc
CatalinBalut <balut.catalin@yahoo.com>
2021-09-08 15:09:30 +0300

    format document

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 const single_digits = {
-    0:"zero", 1:"one", 2:"two", 3:"three", 4:"four",
-    5:"five", 6:"six", 7:"seven", 8:"eight", 9:"nine"
+    0: "zero", 1: "one", 2: "two", 3: "three", 4: "four",
+    5: "five", 6: "six", 7: "seven", 8: "eight", 9: "nine"
 };
-const zero_nineteen_numbers = {10:"ten", 11:"eleven", 12:"twelve", 13:"thirteen", 14:"fourteen", 
-15:"fifteen", 16:"sixteen", 17:"seventeen", 18:"eighteen", 19:"nineteen"};
-
-const rest_of_digits = {20:"twenty", 30:"thirty", 40:"forty", 50:"fifty", 
-60:"sixty", 70:"seventy", 80:"eighty", 90:"ninety"};
-sayNumbers = (number) =>{
-    if(number >= 0 && number <= 9) return single_digits[number]
-    else if(number<=19) return zero_nineteen_numbers[number]
-    else if(number<=90 && number%10===0) return rest_of_digits[number]
-    else if(number<=99) return `${rest_of_digits[number-number%10]} ${single_digits[number%10]}`
+const zero_nineteen_numbers = {
+    10: "ten", 11: "eleven", 12: "twelve", 13: "thirteen", 14: "fourteen",
+    15: "fifteen", 16: "sixteen", 17: "seventeen", 18: "eighteen", 19: "nineteen"
+};
+const rest_of_digits = {
+    20: "twenty", 30: "thirty", 40: "forty", 50: "fifty",
+    60: "sixty", 70: "seventy", 80: "eighty", 90: "ninety"
+};
+sayNumbers = (number) => {
+    if (number >= 0 && number <= 9) {
+        return single_digits[number]
+    } else if (number <= 19) {
+        return zero_nineteen_numbers[number]
+    } else if (number <= 90 && number % 10 === 0) {
+        return rest_of_digits[number]
+    } else if (number <= 99) {
+        return `${rest_of_digits[number - number % 10]} ${single_digits[number % 10]}`
+    }
 }
 
 module.exports = sayNumbers
--- test/app.js
+++ test/app.js
@@ -1,19 +1,19 @@
 const { expect } = require("chai")
 const sayNumbers = require("../src/app")
 describe("ALL TESTS", () => {
-    it("Test the 0 case", () =>{
+    it("Test the 0 case", () => {
         expect(sayNumbers(0)).to.be.equal("zero")
     })
-    it("Expect to be 7", ()=>{
+    it("Expect to be 7", () => {
         expect(sayNumbers(7)).to.be.equal("seven")
     })
-    it("Expect to be 19", ()=>{
+    it("Expect to be 19", () => {
         expect(sayNumbers(19)).to.be.equal("nineteen")
     })
-    it("Expect to be 80", ()=>{
+    it("Expect to be 80", () => {
         expect(sayNumbers(80)).to.be.equal("eighty")
     })
-    it("Expect to be 87", ()=>{
+    it("Expect to be 87", () => {
         expect(sayNumbers(87)).to.be.equal("eighty seven")
     })
 })
